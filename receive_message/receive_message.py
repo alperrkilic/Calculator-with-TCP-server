8110fbb8a4f5df2a54edc2dde39ae84730089477
alperrkilic <balper78645@hotmail.com>
2023-07-06 15:17:27 +0300

    first commit

---
+++ receive_message/receive_message.py
@@ -0,0 +1,40 @@
+import socket
+import json
+
+HOST = 'localhost'
+PORT = 5000
+
+
+def send_data(data):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        client_socket.connect((HOST, PORT))  # Connect to the server
+        client_socket.sendall(data.encode())  # Send the data to the server
+
+        # Receive the response from the server
+        response = client_socket.recv(1024)
+        # Parse the received JSON response (parses into dictionary)
+        response_data = json.loads(response.decode())
+
+        return response_data
+
+
+def menu():
+    choice = ""
+
+    while choice != 'N' or choice != 'n':
+        data_to_send = input("Please enter the operation to calculate: ")
+        response = send_data(data_to_send)
+
+        if response:
+            if response["error"] != "True":
+                print("Result is: ", response["data"])
+                print(response)
+            elif response["error"] == "True":
+                print("Wrong input, please try again.")
+
+        choice = input("Do you want to continue (Y/N) ?: ")
+
+
+menu()
+
+# print(type(response))  # Process the received response (type = dictionary)
---
+++ server/tcp_server.py
@@ -0,0 +1,52 @@
+import socket
+import json
+
+HOST = 'localhost'  # hostname or IP address
+PORT = 5000  # specifying the PORT
+
+
+def accept_client(client_socket):
+    while True:
+        data = client_socket.recv(1024)  # receive max 1024 bytes
+
+        if not data:
+            break  # if no more data , client has disconnected break the loop
+
+        display = data.decode().strip()
+
+        try:
+            result = eval(display)
+            response = {
+                'data': result,
+                'error': False
+            }
+        except:
+            result = display
+            response = {
+                'data': result,
+                'error': True
+            }
+
+        response_json = json.dumps(response)
+        client_socket.sendall(response_json.encode())
+
+    # close the connection if there's no data meaning that client has disconnected
+    client_socket.close()
+
+
+def start_server():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        server_socket.bind((HOST, PORT))
+        server_socket.listen()
+        print("Server listening on {}:{}".format(HOST, PORT))
+
+        while True:
+            client_socket, client_address = server_socket.accept()
+            print("Connected client : {}:{}".format(
+                client_address[0], client_address[1]))
+            # Connected client : 127.0.0.1:53344
+
+            accept_client(client_socket)
+
+
+start_server()
